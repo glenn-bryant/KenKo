49d821bd113f544f89353a172c1fbb95b7c6a416
Glenn Bryant <glennbryant3@gmail.com>
2021-04-02 20:58:33 +0900

    created controllers and views for pages, users, and clinics

---
+++ app/controllers/clinics_controller.rb
@@ -0,0 +1,19 @@
+class ClinicsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ app/views/clinics/create.html.erb
@@ -0,0 +1,2 @@
+<h1>Clinics#create</h1>
+<p>Find me in app/views/clinics/create.html.erb</p>
