59d1cbb50fbe53555a5719e7b2e123228d59a31f
Glenn Bryant <glennbryant3@gmail.com>
2021-04-02 20:46:13 +0900

    created model for clinic, added association to user and validations for name, telephone, hours, address, eng_op, and category

---
+++ app/models/clinic.rb
@@ -0,0 +1,10 @@
+class Clinic < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :telephone, presence: true
+  validates :hours, presence: true
+  validates :address, presence: true
+  validates :eng_op, presence: true
+  validates :category, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, uniqueness: true
-  validates :password, presence: true
-  validates :username, presence: true
   validates :location, presence: true
 
+  has_many :clinics
 end
