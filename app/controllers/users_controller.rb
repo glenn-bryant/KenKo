7ee6a2572482a10aeb3b09dd6a5c3ffee402b312
Glenn Bryant <glennbryant3@gmail.com>
2021-04-17 11:52:44 +0900

    testing geocode in production environment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    if Rails.env.production?
+      @user.location = request.location.country
+    end
   end
 
   def update
