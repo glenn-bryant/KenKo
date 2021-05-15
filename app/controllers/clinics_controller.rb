b16a72eea5c60003a467e8da29e113af67309edd
Glenn Bryant <glennbryant3@gmail.com>
2021-05-15 19:39:30 +0900

    modified search feature so users can now search by address or category

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -4,7 +4,8 @@ Mapbox.access_token = "pk.eyJ1IjoiZ2dsZW5uODgiLCJhIjoiY2tuaGM5MDBzMHpqejJubndudT
 class ClinicsController < ApplicationController
   def index
     if params[:query].present?
-      @clinics = Clinic.where("address ILIKE ?", "%#{params[:query]}%")
+      sql_query = "address ILIKE :query OR category ILIKE :query"
+      @clinics = Clinic.where(sql_query, query: "%#{params[:query]}%")
     else
       @clinics = Clinic.all
     end
